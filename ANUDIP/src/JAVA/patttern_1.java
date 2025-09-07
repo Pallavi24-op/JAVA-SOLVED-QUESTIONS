4255b8eb933339591153b83b4695a16909ce3332
Pallavi <Pallavi@LAPTOP-U3GFNC63>
2025-09-07 15:46:43 +0530

    COMMIT

---
+++ ANUDIP/src/JAVA/patttern_1.java
@@ -0,0 +1,32 @@
+package JAVA;
+
+public class patttern_1 {
+/*
+	*****
+	*   *
+	*   *
+	*   *
+	*****
+
+ */
+public static void main(String args []) {
+		int row=5;
+		int column=5; 
+		for(int i=0;i<row;i++) 
+		{  
+			for(int j=0;j<column;j++) 
+			{ 
+			if(i==0||j==0||i==row-1||j==column-1) 
+				{
+			System.out.print('*');	
+				}
+			else {
+			System.out.print(' ');		
+				}
+	}	
+		System.out.println();
+	}
+	}
+	}
+
+
